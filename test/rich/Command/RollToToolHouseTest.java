485b1e6d37fabb0ec5e6ffb7b955dad92bf47d8e
Siyue Zhang <syzhang@thoughtworks.com>
2016-11-14 23:47:25 +0800

    should wait response/end turn roll to tool house

---
+++ test/rich/Command/RollToToolHouseTest.java
@@ -0,0 +1,56 @@
+package rich.Command;
+
+import org.junit.Before;
+import org.junit.Test;
+import rich.Dice;
+import rich.Map;
+import rich.Place.Place;
+import rich.Place.Prison;
+import rich.Place.ToolHouse;
+import rich.Player;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RollToToolHouseTest {
+    private Map map;
+    private Dice dice;
+    private Place toolHouse;
+    private Place starting;
+
+    private RollCommand rollCommand;
+
+    @Before
+    public void setUp() throws Exception {
+        map = mock(Map.class);
+        dice = mock(Dice.class);
+        when(dice.next()).thenReturn(1);
+        toolHouse = new ToolHouse();
+        starting = mock(Place.class);
+        when(map.move(any(), eq(1))).thenReturn(toolHouse);
+
+        rollCommand = new RollCommand(map, dice);
+    }
+
+    @Test
+    public void should_wait_response_when_arrive_tool_house() throws Exception {
+        Player player = Player.createPlayerWithPoints(toolHouse, 100);
+
+        player.executeCommand(rollCommand);
+
+        assertThat(player.getStatus(), is(Player.Status.WAIT_RESPONSE));
+    }
+
+    @Test
+    public void should_end_turn_when_arrive_tool_house_without_enough_points() throws Exception {
+        Player player = Player.createPlayerWithPoints(toolHouse, ToolHouse.POINT_LIMIT - 1);
+
+        player.executeCommand(rollCommand);
+
+        assertThat(player.getStatus(), is(Player.Status.END_TURN));
+    }
+}
